5a7445d831edacec4d5d3891dd1467c572d091ca
VitKhomen <vital210890@gmail.com>
2025-03-28 19:12:46 +0200

    Prepare app to deploy

--- django_cours/settings.py
+++ django_cours/settings.py
@@ -30,7 +30,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0', 'localhost',
+                 'itvdn-test.herokuapp.com']
 
 SESSION_COOKIE_SECURE = True
 
